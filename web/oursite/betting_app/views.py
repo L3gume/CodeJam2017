843a1539192b2b05fee73218093549d914954b90
Michael Vaquier <michael.vaquier@mail.mcgill.ca>
2017-11-18 11:07:35 -0500

    Base html file added

--- web/oursite/betting_app/views.py
+++ web/oursite/betting_app/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html', {
+        'foo': 'bar',
+    }, content_type='application/xhtml+xml')
