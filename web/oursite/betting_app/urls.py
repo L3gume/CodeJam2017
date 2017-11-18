762bcf5426e3d10a6c138db024c40ea3b32e997a
Bogdan Dumitru <bogdan.dumitru@mail.mcgill.ca>
2017-11-18 15:18:17 -0500

    stuff

--- web/oursite/betting_app/urls.py
+++ web/oursite/betting_app/urls.py
@@ -6,8 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^start', views.start_league, name='start_league'),
     url(r'^bidding', views.bidding, name='bidding')
-    # url(r'^signup/', views),
-    # url(r'^bidder_standings/')
-    # url(r'^baseball_standings/')
 
 ]
--- web/oursite/betting_app/views.py
+++ web/oursite/betting_app/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 def bidding(request):
+    boii = request.POST.get('players', None)
 
-    return HttpResponse("Bidding")
+    return HttpResponse(boii)
 def start_league(request):
     return render(request, 'start.html')
