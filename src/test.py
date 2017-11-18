7018f07664c42f5c9e67b057f9f02da06d061c4c
MrL3gume <justin.tremblay@mail.mcgill.ca>
2017-11-18 13:49:13 -0500

    testing tings

--- src/test.py
+++ src/test.py
@@ -1,8 +1,11 @@
 import predict_season as sp
 import team_data as td
+import team_parser as tp
 
 #p = sp.Season_Predictor()
 #p.train()
 
+t = tp.team_parser()
+
 np = sp.Season_Predictor()
-data = np.get_prediction(2018, 878378)
+data = np.get_prediction(1995, t.str_to_nb('WSN'))
