406da2f9aa674f2d86dace85507bc88b086d59bd
MrL3gume <justin.tremblay@mail.mcgill.ca>
2017-11-19 10:14:13 -0500

    Updated game_manager and team_data

--- web/oursite/betting_app/team_data.py
+++ web/oursite/betting_app/team_data.py
@@ -1,4 +1,11 @@
 class team_data:
+    def __init__(self):
+        self.year_id = 0
+        self.team_id = ''
+        self.rank = 0
+        self.wins = 0
+        self.losses = 0
+
     def __init__(self, _year_id, _team_id, _rank, _wins, _losses):
         self.year_id = _year_id
         self.team_id = _team_id
