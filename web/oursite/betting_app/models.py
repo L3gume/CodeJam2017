31a2c3a3818fa696434a6f4aa1f82eaeaff57bb4
Bogdan Dumitru <bogdan.dumitru@mail.mcgill.ca>
2017-11-18 19:27:41 -0500

    Reorganized some stuff

--- web/oursite/betting_app/models.py
+++ web/oursite/betting_app/models.py
@@ -10,12 +10,5 @@ class Player(models.Model):
     amount_bet = models.IntegerField(default = 0)
     team_bet = models.CharField(max_length = 3, default = "")
 
-    # def __init__(self, PID=0, wins=0, loses=0, wallet=100, amount_bet=0, team_bet=""):
-    #     self.PID = PID   #The player's ID
-    #     self.wins = wins  #The total number of wins of the player
-    #     self.loses = loses #The total number of losses of the player
-    #     self.wallet = wallet #The player wallet storing how much money they have left to bet
-    #     self.amount_bet = amount_bet #The amount of money the player bet this round
-    #     self.team_bet = team_bet #The team the player is currently betting on
     def __str__(self):
         return "PID: {}\nName: {}\n# Wins: {}\n# Loses: {}\nWallet: {}\nAmount Bet: {}\nTeam Bet: {}\n".format(self.pid, self.player_name, self.wins, self.losses, self.wallet, self.amount_bet, self.team_bet)
