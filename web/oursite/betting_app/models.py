13530fa0e3d7ca97351c6fe6c232168b5db56841
Bogdan Dumitru <bogdan.dumitru@mail.mcgill.ca>
2017-11-18 19:11:00 -0500

    Reorganized some stuff

--- web/oursite/betting_app/models.py
+++ web/oursite/betting_app/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class Player(models.Model):
 
-    pid = models.IntegerField()
-    wins = models.IntegerField()
-    losses = models.IntegerField()
-    wallet = models.IntegerField()
-    amount_bet = models.IntegerField()
-    team_bet = models.CharField(max_length = 3)
-    player_name = models.CharField(max_length = 32)
+    pid = models.IntegerField(default = 0)
+    player_name = models.CharField(max_length = 32, default = "")
+    wins = models.IntegerField(default = 0)
+    losses = models.IntegerField(default = 0)
+    wallet = models.IntegerField(default = 0)
+    amount_bet = models.IntegerField(default = 0)
+    team_bet = models.CharField(max_length = 3, default = "")
 
     # def __init__(self, PID=0, wins=0, loses=0, wallet=100, amount_bet=0, team_bet=""):
     #     self.PID = PID   #The player's ID
@@ -18,4 +18,4 @@ class Player(models.Model):
     #     self.amount_bet = amount_bet #The amount of money the player bet this round
     #     self.team_bet = team_bet #The team the player is currently betting on
     def __str__(self):
-        return "PID: {}\n# Wins: {}\n# Loses: {}\nWallet: {}\nAmount Bet: {}\nTeam Bet: {}\n".format(self.pid, self.wins, self.losses, self.wallet, self.amount_bet, self.team_bet)
+        return "PID: {}\nName: {}\n# Wins: {}\n# Loses: {}\nWallet: {}\nAmount Bet: {}\nTeam Bet: {}\n".format(self.pid, self.player_name, self.wins, self.losses, self.wallet, self.amount_bet, self.team_bet)
